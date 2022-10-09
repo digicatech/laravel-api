42b16c8202675bb84cc12cdc510cc32b44f9acb9
Digicatech <info@digicatech.com>
2022-10-09 22:46:20 +0300

    Update with Put and PAtch

---
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+
+class UpdateCustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $method = $this->method();
+
+        if ($method == 'PUT') {
+            return [
+                'name' => ['required'],
+                'type' => ['required' , Rule::in(['I','B'])],
+                'email' => ['required','email'],
+                'address' => ['required'],
+                'city' => ['required'],
+                'state' => ['required'],
+                'zip' => ['required']
+            ];
+        }
+        else {
+            return [
+                'name' => ['sometimes','required'],
+                'type' => ['sometimes','required' , Rule::in(['I','B'])],
+                'email' => ['sometimes','required','email'],
+                'address' => ['sometimes','required'],
+                'city' => ['sometimes','required'],
+                'state' => ['sometimes','required'],
+                'zip' => ['sometimes','required']
+            ];
+        }
+        
+    }
+
+
+    protected function prepareForValidation()
+    {
+        if ($this->zip) {
+            $this->merge([
+                'postal_code' => $this->zip
+            ]);
+        }
+        
+    }
+}

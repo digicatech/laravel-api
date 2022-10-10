c2ba89d1bcd0fc613beabd301299e9bb57ef9dcd
Digicatech <info@digicatech.com>
2022-10-10 21:59:07 +0300

    creaitng token

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/setup',function(){
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => '123456'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new User();
+
+        $user->name = "Admin";
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-tokem', ['create','update','delete']);
+            $updateToken = $user->createToken('update-tokem', ['create','update']);
+            $basicToken = $user->createToken('basic-tokem');
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken
+            ];
+        }
+    }
 });
